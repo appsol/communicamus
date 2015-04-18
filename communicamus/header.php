dcdcf6ba8fad1214960eb0da251eb345df39a1f1
Stuart Laverick <appsol@gmail.com>
2015-04-18 23:14:23 +0100

    changed the text domain from _s to communicamus

--- communicamus/header.php
+++ communicamus/header.php
@@ -26,7 +26,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', '_s' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'communicamus' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
 <?php if (is_active_sidebar('header-top')): ?>
